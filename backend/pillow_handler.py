5299e92456d4441b2aa5db3e62d75adb7462b45e
Rommel John Ronduen <rommel.ronduen2244@gmail.com>
2026-01-16 17:00:43 +0800

    fixed pillow_handler typo on b64 decode

--- backend/pillow_handler.py
+++ backend/pillow_handler.py
@@ -28,7 +28,7 @@ def decode_base64_to_pillow(base64_string: str) -> Image.Image:
     if "," in base64_string:
         base64_string = base64_string.split(",")[1]
 
-    img_bytes = base64_b64decode(base64_string)
+    img_bytes = base64.b64decode(base64_string)
     img_buffer = io.BytesIO(img_bytes)
     pil_img = Image.open(img_buffer)
 
