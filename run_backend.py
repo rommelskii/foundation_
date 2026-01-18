d1f4a7c6a2081ec9aee2e41ee7d70df069ae70bb
Rommel John Ronduen <rommel.ronduen2244@gmail.com>
2026-01-18 22:42:20 +0800

    added cross-site policy

--- run_backend.py
+++ run_backend.py
@@ -1,8 +1,10 @@
 from backend.app import create_app
 from backend.app.utils.env_helper import EnvVars
+from flask_cors import CORS
 
 envs = EnvVars()
 app = create_app()
+CORS(app)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=envs.API_PORT)
